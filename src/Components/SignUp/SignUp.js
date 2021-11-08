bd068cc0cd0b92ae774cd6d90b2505bd60980c9a
Calil-Silva <ka.renner@hotmail.com>
2021-11-08 19:05:51 -0300

    Update

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -2,24 +2,40 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { country_arr } from "../../Data/countris";
-import { BsCheck } from "react-icons/bs";
+import { SiHandshake } from "react-icons/si";
 import CountriesList from "./CountriesList";
 
 export default function SignUp() {
   const [openContries, setOpenCantries] = useState(false);
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
 
-  console.log(selectedCountry);
+  console.log(
+    selectedCountry,
+    birthDate,
+    name,
+    surname,
+    email,
+    password,
+    confirmedPassword
+  );
 
   return (
     <>
       <HeaderContainer>
         <HeaderWrapper>
-          <h1>ID TechCommerce</h1>
+          <Link to="/home">
+            <h1>ID TechCommerce</h1>
+          </Link>
           <LinksWrapper>
-            <PageLInk to>Iniciar sessão</PageLInk>
-            <PageLInk>Crie seu ID TechCommerce</PageLInk>
-            <PageLInk>Perguntas frequentes</PageLInk>
+            <PageLInk to="/signin">Iniciar sessão</PageLInk>
+            <PageLInk to="/signup">Crie seu ID</PageLInk>
+            <PageLInk to="/signup">Perguntas frequentes</PageLInk>
           </LinksWrapper>
         </HeaderWrapper>
       </HeaderContainer>
@@ -29,18 +45,29 @@ export default function SignUp() {
         da TechCommerce.
       </SubTitle>
       <SubTitle>
-        Já tem um ID TechCommerce? &nbsp;
-        <a href>Encontre-o aqui</a>
+        <span>Já tem um ID TechCommerce? &nbsp;</span>
+        <Link to="signin">
+          <span>Entrar</span>
+        </Link>
       </SubTitle>
       <OuterDiv>
         <Container>
           <Form>
             <Name>
-              <input placeholder="Nome" type="text" />
-              <input placeholder="Sobrenome" type="text" />
+              <input
+                placeholder="Nome"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                placeholder="Sobrenome"
+                type="text"
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
+              />
             </Name>
-
-            <Location>
+            <LocationSection>
               <span>PAÍS/REGIÃO</span>
               <input
                 placeholder="País"
@@ -48,10 +75,49 @@ export default function SignUp() {
                 onFocus={() => setOpenCantries(true)}
                 value={selectedCountry}
               />
-              <input placeholder="Data de nascimento" type="date" />
-            </Location>
+              <span>Data de nascimento</span>
+              <input
+                placeholder="Data de nascimento"
+                type="text"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
+            </LocationSection>
           </Form>
         </Container>
+        <Container>
+          <LoginSection>
+            <input
+              placeholder="nome@exemplo.com"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <span>Este será o seu novo ID TechCommerce</span>
+            <input
+              placeholder="Senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              placeholder="Confirmar senha"
+              type="password"
+              value={confirmedPassword}
+              onChange={(e) => setConfirmedPassword(e.target.value)}
+            />
+          </LoginSection>
+        </Container>
+        <SubmitSection>
+          <SiHandshake style={{ fontSize: "3rem" }} />
+          <span>
+            As informações do seu ID Apple são usadas para permitir que você
+            inicie sessão de forma segura e acesse seus dados. A Apple registra
+            determinados dados de uso para fins de segurança, suporte e geração
+            de relatórios. Veja como os seus dados são gerenciados.
+          </span>
+          <input type="submit" value="Continuar" />
+        </SubmitSection>
         <CountriesContainer>
           <CounstriesList openContries={openContries}>
             {country_arr.map((country, index) => (
@@ -122,9 +188,14 @@ const Title = styled.h1`
 const SubTitle = styled.div`
   text-align: center;
   margin-bottom: 0.5rem;
+  padding: 0 1rem;
+  font-size: 0.9rem;
 
-  a {
+  span:last-child {
     color: hsl(241, 100%, 50%);
+    :hover {
+      cursor: pointer;
+    }
   }
 `;
 
@@ -134,20 +205,20 @@ const OuterDiv = styled.div`
 `;
 
 const Container = styled.div`
-  margin: 0 15rem;
   display: flex;
   justify-content: center;
   border-bottom: 1px solid rgb(231, 231, 231);
+  align-items: center;
 `;
 
-const Form = styled.form`
+const Form = styled.div`
   input {
     height: 3.5rem;
-    width: 13rem;
+    max-width: 13rem;
+    width: calc(100vw / 2);
     border-radius: 5px;
     border: 1px solid rgb(219, 219, 219);
-    margin-bottom: 2.5rem;
-    margin-top: 1rem;
+    margin: 1rem 0 2.5rem;
     padding-left: 1rem;
     font-size: 1rem;
     ::placeholder {
@@ -159,10 +230,23 @@ const Form = styled.form`
 
 const Name = styled.div`
   display: flex;
-  column-gap: 1rem;
+  justify-content: space-between;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+
+    input {
+      width: calc(100vw - 2rem);
+      max-width: 27rem;
+
+      &:last-child {
+        margin: -1.5rem 0 2rem;
+      }
+    }
+  }
 `;
 
-const Location = styled.div`
+const LocationSection = styled.div`
   display: flex;
   flex-direction: column;
 
@@ -173,12 +257,12 @@ const Location = styled.div`
   }
 
   input {
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-    width: 27rem;
+    margin: 0.5rem 0 1rem;
+    width: calc(100vw - 2rem);
+    max-width: 27rem;
 
     &:last-child {
-      margin-top: 0;
+      margin-top: 0.5rem;
       margin-bottom: 2rem;
     }
   }
@@ -186,15 +270,16 @@ const Location = styled.div`
 
 const CountriesContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
 `;
 
 const CounstriesList = styled.ul`
   display: ${({ openContries }) => (!openContries ? "none" : "initial")};
-  width: 25rem;
-  height: 100vh;
+  width: calc(100vw - 2rem);
+  max-width: 25rem;
+  height: calc(100% - 1rem);
   background-color: yellow;
   position: absolute;
   top: 0;
@@ -213,4 +298,66 @@ const CounstriesList = styled.ul`
   scrollbar-width: none;
   box-shadow: 0 0 1em black;
   border: 0.1px solid white;
+`;
+
+const LoginSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  input {
+    width: calc(100vw - 2rem);
+    max-width: 27rem;
+    height: 3.5rem;
+    border-radius: 5px;
+    border: 1px solid rgb(219, 219, 219);
+    margin: 1rem 0 0;
+    padding-left: 1rem;
+    font-size: 1rem;
+    ::placeholder {
+      color: rgb(129, 129, 129);
+      font-size: 1rem;
+    }
+
+    &:first-child {
+      margin-top: 2rem;
+      margin-bottom: 0.5rem;
+    }
+
+    &:last-child {
+      margin-bottom: 2rem;
+    }
+  }
+
+  span {
+    color: rgb(119, 119, 119);
+    font-size: 0.9rem;
+  }
+`;
+
+const SubmitSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0 1rem;
+  span {
+    max-width: 27rem;
+    text-align: center;
+    color: rgb(119, 119, 119);
+    font-size: 0.8rem;
+    margin: 1rem;
+  }
+
+  input {
+    height: 2.2rem;
+    width: 7rem;
+    border-radius: 5px;
+    border: none;
+    background-image: linear-gradient(hsl(206, 86%, 63%), hsl(206, 56%, 44%));
+    font-size: 1rem;
+    cursor: pointer;
+    color: white;
+    :hover {
+      background-image: linear-gradient(hsl(206, 86%, 73%), hsl(206, 56%, 54%));
+    }
+  }
 `;
